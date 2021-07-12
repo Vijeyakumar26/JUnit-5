1719c1ebbfba6b9242cd8d4df785c357e5473ae1
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-12 22:30:08 +0530

    Created MathUtilsTest

---
+++ junit-5-jb/src/test/java/com/junit/MathUtilsTest.java
@@ -0,0 +1,30 @@
+package com.junit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Test;
+
+public class MathUtilsTest {
+
+	@Test
+	public void testAdd() {
+		MathUtils mathUtils = new MathUtils();
+		int actual = mathUtils.add(1, 1);
+		int expected = 2;
+		
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testComputeCircleArea() {
+		MathUtils mathUtils = new MathUtils();
+		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10));
+	}
+	
+	@Test
+	public void testDivide() {
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(NullPointerException.class,() -> mathUtils.divide(1, 0));
+	}
+}
