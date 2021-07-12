ccc4c8f3e83c94257547558cefca7d4560689c91
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-12 22:31:42 +0530

    Created GreetingImplementation

---
+++ lambdas/src/com/lambdas/GreetingImplementation.java
@@ -0,0 +1,11 @@
+package com.lambdas;
+
+public class GreetingImplementation implements Greeting{
+
+	@Override
+	public void perform() {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+																
+}
