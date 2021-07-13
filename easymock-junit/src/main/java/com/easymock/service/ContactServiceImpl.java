8a955f4d5a4c38f41bcf1b0555198266c71824b1
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-13 17:04:07 +0530

    Generated Getters Setters for ContactDAO

--- easymock-junit/src/main/java/com/easymock/service/ContactServiceImpl.java
+++ easymock-junit/src/main/java/com/easymock/service/ContactServiceImpl.java
@@ -6,6 +6,14 @@ public class ContactServiceImpl implements ContactService {
 
 	private ContactDAO contactdao;
 	
+	public ContactDAO getContactdao() {
+		return contactdao;
+	}
+
+	public void setContactdao(ContactDAO contactdao) {
+		this.contactdao = contactdao;
+	}
+
 	public String getNameById(Integer id) {
 		
 		String name = contactdao.findNameById(id);
