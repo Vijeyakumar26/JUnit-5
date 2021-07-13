bd854902c66dae216c0f04f0108fdef7623b9655
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-13 21:09:14 +0530

    Created BaeldungReader

---
+++ easymock-baeldung/src/main/java/com/easymock/BaeldungReader.java
@@ -0,0 +1,27 @@
+package com.easymock;
+
+import java.util.List;
+
+public class BaeldungReader {
+
+	private ArticleReader articleReader;
+	private IArticleWriter articleWriter;
+
+	// constructors
+
+	public BaeldungReader(ArticleReader mockArticleReader) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public BaeldungArticle readNext(){
+		return articleReader.next();
+	}
+
+	public List<BaeldungArticle> readTopic(String topic){
+		return articleReader.ofTopic(topic);
+	}
+
+	public String write(String title, String content){
+		return articleWriter.write(title, content);
+	}
+}
