d296a1b5724cef998ca34f340e1a10d2580a613f
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-13 16:40:47 +0530

    Renamed ContactServiceTest

--- easymock-junit/src/test/java/com/easymock/service/ContactServiceTest.java
+++ easymock-junit/src/test/java/com/easymock/service/ContactServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class ContactServiceTest1 {
+public class ContactServiceTest {
 
 	@BeforeEach
 	void setUp() throws Exception {
