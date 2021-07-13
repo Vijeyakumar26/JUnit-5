e8998d2d5607a438f6daeb57eb651a95f905fbbf
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-13 21:08:09 +0530

    Created ArticleReader

---
+++ easymock-baeldung/src/main/java/com/easymock/ArticleReader.java
@@ -0,0 +1,17 @@
+package com.easymock;
+
+import java.util.List;
+
+public class ArticleReader {
+
+	public BaeldungArticle next() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<BaeldungArticle> ofTopic(String topic) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
