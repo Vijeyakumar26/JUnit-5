136c486542f1c0076be634cf9012fb0d294437d0
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-14 21:59:28 +0530

    Mock methods

--- easymock-journalDev/src/test/java/com/easymock/EasyMockMethodExample.java
+++ easymock-journalDev/src/test/java/com/easymock/EasyMockMethodExample.java
@@ -1,7 +1,11 @@
 package com.easymock;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -13,7 +17,21 @@ public class EasyMockMethodExample {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+		ArrayList<Integer> mockList = EasyMock.mock(ArrayList.class);
+		EasyMock.expect(mockList.add(10)).andReturn(true);
+		EasyMock.expect(mockList.add(20)).andReturn(true);
+		EasyMock.expect(mockList.size()).andReturn(2);
+		EasyMock.expect(mockList.get(0)).andReturn(10);
+		EasyMock.replay(mockList);
 
+		mockList.add(10);
+		mockList.add(20);
+//		below will throw exception because it's not mocked
+		mockList.add(30);
+
+		assertTrue(mockList.get(0) == 10);
+
+		assertEquals(mockList.size(), 2);
+
+	}
 }
