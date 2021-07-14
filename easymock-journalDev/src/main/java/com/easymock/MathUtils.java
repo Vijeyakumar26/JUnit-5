3be031323fb9f68a0202e7aab39784297ba66b32
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-14 16:26:22 +0530

    MathUtils with Calculator Loosely Coupled

---
+++ easymock-journalDev/src/main/java/com/easymock/MathUtils.java
@@ -0,0 +1,19 @@
+
+package com.easymock;
+
+public class MathUtils {
+
+	private Calculator calc;
+	
+	public MathUtils(Calculator c) {
+		this.calc = c;
+	}
+	
+	public int add(int i, int j) {
+		return this.calc.add(i, j);
+	}
+	
+	public int multiply(int i, int j) {
+		return this.calc.multiply(i, j);
+	}
+}
