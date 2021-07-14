4c0ce424c7b241a7818024d2a1f41be189baaa9a
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-07-14 18:42:14 +0530

    EasyMockMethodExample JUnit

---
+++ easymock-journalDev/src/test/java/com/easymock/EasyMockMethodExample.java
@@ -0,0 +1,19 @@
+package com.easymock;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EasyMockMethodExample {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
