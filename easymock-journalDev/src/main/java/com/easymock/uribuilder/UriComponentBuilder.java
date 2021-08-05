402b171d067fc0585ac0123f088b018fd3bc5298
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-08-05 20:03:51 +0530

    added UriComponentsBuilder in Spring

--- easymock-journalDev/src/main/java/com/easymock/uribuilder/UriComponentBuilder.java
+++ easymock-journalDev/src/main/java/com/easymock/uribuilder/UriComponentBuilder.java
@@ -1,12 +1,19 @@
 package com.easymock.uribuilder;
 
+import org.springframework.web.util.UriComponentsBuilder;
+
 public class UriComponentBuilder {
 	
-	public boolean handleScheduler(String rowkey,String process) {
+	public static boolean handleScheduler(String rowkey,String process) {
 		
-		String url = "";
+		String url = UriComponentsBuilder.fromPath("/syslogReporting/handleScheduler")
+						.queryParam("rowkey", rowkey).queryParam("process", process).build().toUriString();
+		System.out.println(url);
 		return false;
 		
+	}
+	public static void main(String[] args) {
+		UriComponentBuilder.handleScheduler("rowkey-rowkey-rowkey", "hold");
 	}
 
 }
