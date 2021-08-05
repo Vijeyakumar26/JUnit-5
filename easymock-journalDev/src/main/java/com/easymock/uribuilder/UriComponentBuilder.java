15cacec59b3f8573aa801b7441b8c243e4e9dfdc
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-08-05 19:56:06 +0530

    Created UriComponentBuilder

---
+++ easymock-journalDev/src/main/java/com/easymock/uribuilder/UriComponentBuilder.java
@@ -0,0 +1,12 @@
+package com.easymock.uribuilder;
+
+public class UriComponentBuilder {
+	
+	public boolean handleScheduler(String rowkey,String process) {
+		
+		String url = "";
+		return false;
+		
+	}
+
+}
