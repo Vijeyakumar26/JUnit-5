38c7dbdc084423b11289ffd60b99614cd1d5d6db
Vijeyakumar R <vijeyakumar.26@gmail.com>
2021-08-06 17:30:31 +0530

    Updated with ReflectionTest Methods

--- junit-5-jb/src/test/java/com/junit/employee/EmployeeTest.java
+++ junit-5-jb/src/test/java/com/junit/employee/EmployeeTest.java
@@ -1,24 +1,42 @@
 package com.junit.employee;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.junit.Employee.Employee;
+import com.junit.Employee.RestService;
 
 public class EmployeeTest {
 
 	Employee emp;
+	RestService restService;
 	
 	@Before
 	public void setUp() throws Exception {
 		emp = new Employee();
+		restService = EasyMock.createMock(RestService.class);
+		ReflectionTestUtils.setField(emp, "restService", restService);
 	}
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
-	}
+		Employee employee = new Employee();
+		ReflectionTestUtils.setField(employee, "id", 1);
 
+		assertTrue(employee.getId().equals(1));
+	}
+	
+	@Test
+	public void reflectionTest()
+	{
+		ReflectionTestUtils.setField(emp, "restService", restService);
+		assertEquals(0, 0);
+	}
 }
+
+
